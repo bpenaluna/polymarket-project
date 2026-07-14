63c73d56d20a7fbce32745986d776f4198520ac6
bpenaluna <ben.penaluna@gmail.com>
2026-07-14 23:41:28 +0100

    CSS to dashboard

--- dashboard/app.py
+++ dashboard/app.py
@@ -2,14 +2,27 @@ import duckdb
 import pandas as pd
 import plotly.express as px
 from dash import Dash, dcc, html
+import dash_mantine_components as dmc
 from dash.dependencies import Input, Output
 
 app = Dash(__name__)
-app.layout = html.Div([
-    html.H1("BTC Price / Prediction", style={'textAlign':'center'}),
-    dcc.Graph(id="polymarket-price"),
-    dcc.Graph(id="btc-price"),
-    dcc.Interval(id="interval", interval=30_000, n_intervals=0)  # refresh every 30s
+
+# app.layout = html.Div([
+#     html.H1("BTC Price / Prediction", style={'textAlign':'center'}),
+#     dcc.Graph(id="polymarket-price"),
+#     dcc.Graph(id="btc-price"),
+#     dcc.Interval(id="interval", interval=2_000, n_intervals=0)  # refresh every 10s
+# ])
+
+app.layout = dmc.MantineProvider([
+    dmc.Container([
+        dmc.Title("BTC Price / Prediction Dashboard", c="blue", order=3),
+        dmc.SimpleGrid([
+            dcc.Graph(id="polymarket-price"),
+            dcc.Graph(id="btc-price")
+        ], cols={"base": 1, "md": 2})
+    ], fluid=True),
+    dcc.Interval(id="interval", interval=2_000, n_intervals=0)  # refresh every 2s
 ])
 
 @app.callback(Output("polymarket-price", "figure"), Input("interval", "n_intervals"))
